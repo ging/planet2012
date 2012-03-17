37df8e02cce7975af25f3ff0ff60360fa4247519
Juan Quemada <jquemada@dit.upm.es>
2012-03-18 00:06:27 +0100

    vistas index y contact, y redirección de route

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,8 @@ Planet::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  
+  root :to => "planet#index"
 
   # See how all your routes lay out with "rake routes"
 
