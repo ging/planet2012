c5aade1b24ba2cf926e0a2e8d051191b20616ccb
Juan Quemada <jquemada@dit.upm.es>
2012-03-18 00:12:51 +0100

    añadir acción ejemplo

--- app/controllers/planet_controller.rb
+++ app/controllers/planet_controller.rb
@@ -4,4 +4,8 @@ class PlanetController < ApplicationController
 
   def contact
   end
+
+  def ejemplo
+  end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Planet::Application.routes.draw do
 
   get "planet/contact"
 
+  get "planet/ejemplo"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -53,12 +55,12 @@ Planet::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  
+    
   root :to => "planet#index"
-
+    
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  # match ':controller(/:action(/:id(.:format)))'
 end
