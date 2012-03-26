cc1ce802e7c14d91fa926183128ef556671a630a
Fausto Rodrigues <f4usto@gmail.com>
2012-03-26 05:21:56 +0200

    New Planet w/ MySQL

--- Gemfile
+++ Gemfile
@@ -5,7 +5,8 @@ gem 'rails', '3.2.2'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3'
+gem 'mysql2', '0.2.7'
+gem 'activerecord'
 
 
 # Gems used only for assets and not required
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Planet2012::Application.config.secret_token = '302a6f7c01ad8410db88b1830880081ba3b8c052b80e8b3baaf9820e261a1013cbfc9828164eb7e523d11df0f4a8e9b488acbb36c3723545d051ac33c2665b9a'
+Planet2012::Application.config.secret_token = '1419b0ac7b4ede8002628c19a699446067da725c41ad2f27e3948d7deb45314d13cfd88a4c36cb2ef576ae855b786b55b6fcdc97e40ba6f841be44ef88a1aaa2'
