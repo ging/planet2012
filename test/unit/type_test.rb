62ae267056815fd519ea00faa2726a6fb5777712
Juan Quemada <jquemada@dit.upm.es>
2012-03-29 00:21:47 +0200

    validaciones y tests corregidos

--- test/unit/type_test.rb
+++ test/unit/type_test.rb
@@ -4,4 +4,12 @@ class TypeTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  # type creado sin atributos es invalido y genera errores
+  test "Type attributes must not be empty" do
+    type = Type.new
+    assert type.invalid?      # Test pasa si validación no pasa 
+    assert type.errors[:name].any?
+    assert type.errors[:description].any?
+  end
 end
