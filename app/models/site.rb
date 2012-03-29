025e06524552c6160e7080dce56a42842bd2779d
Juan Quemada <jquemada@dit.upm.es>
2012-03-29 10:23:59 +0200

    User Sites relation

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,6 +1,10 @@
 class Site < ActiveRecord::Base
   belongs_to :type
+  belongs_to :user
   
+  # Debe estar protegido para evitar accesos indeseados
+  attr_protected :user_id    
+
   # Se añaden estas definiciones 
   validates :name, :type_id, :image_url,    :presence => true   # campo obligatorio
 end
