6e00f2fa11ebc7a0e1d8904b9d18e8ede89bed07
Marina González <m.gonzalezc@alumnos.upm.es>
2012-04-10 20:17:28 +0200

    validaciones y tests corregidos

--- app/models/type.rb
+++ app/models/type.rb
@@ -1,3 +1,6 @@
 class Type < ActiveRecord::Base
      has_many :sites
+     # Se añade esta definición de recurso
+     validates :name, :description, :presence => true # campo obligatorio
+     validates :name, :uniqueness => true # Campo no repetido
 end
