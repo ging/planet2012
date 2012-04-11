33b971fb0099b48295d4ae485a8ac9f2d392c636
Marina González <m.gonzalezc@alumnos.upm.es>
2012-04-11 18:00:59 +0200

    migración trip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120411151143) do
+ActiveRecord::Schema.define(:version => 20120411160020) do
 
   create_table "sites", :force => true do |t|
     t.string   "name"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(:version => 20120411151143) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "user_id"
+  end
+
+  create_table "trips", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "types", :force => true do |t|
