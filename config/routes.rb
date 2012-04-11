224dbb10f991f30c4a6990269de47babf35c68fb
Merlin <merlin@MacBook-Air-de-Merlin.local>
2012-04-11 17:48:22 +0200

    Cambiada vista para sites, ahora aparece el enlace comentarios

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Planet::Application.routes.draw do
   resources :types do                     # Rutas anidadas /types/id/sites...,
     resources :sites, :only => [ :index ] # Restringe a acción “index” 
   end
+
+  resources :sites do                     # Rutas anidadas /types/id/sites...,
+    resources :comentarios, :only => [ :index ] # Restringe a acción “index” 
+  end
   
   get "planet/index"
 
