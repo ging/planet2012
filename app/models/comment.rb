32ad3776edb0640fab35b843466559395448782c
Marina González <m.gonzalezc@alumnos.upm.es>
2012-04-13 20:33:59 +0200

    Mejora de vistas para añadir comentarios

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,7 @@
 class Comment < ActiveRecord::Base
+
+   belongs_to :user
+   belongs_to :site
+
+   attr_protected :user_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   has_many :sites 
   has_many :trips
 
+  has_many :comments
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
