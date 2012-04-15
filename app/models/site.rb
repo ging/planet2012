cd3b34d9c1f786338c751c64544cd7f2bb61663c
lmargal06 <luismg87@gmail.com>
2012-04-15 17:58:25 +0200

    validaciones y test corregidos

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,3 +1,11 @@
 class Site < ActiveRecord::Base
   belongs_to :type
+  
+
+  
+  # Debe estar protegido para evitar accesos indeseados
+   
+
+  # Se añaden estas definiciones 
+  validates :name, :type_id,:image_url,    :presence => true   # campo obligatorio
 end
