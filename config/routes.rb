91c7d5c06e988598f1d2b7feb8838551b5e2afc3
lmargal06 <luismg87@gmail.com>
2012-04-15 18:04:58 +0200

    instalar devise,user y nombre

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Planet::Application.routes.draw do
   
+  devise_for :users
+
   resources :sites
 
   get "types/ordered_index"
---
+++ db/migrate/20120415160149_user_name.rb
@@ -0,0 +1,13 @@
+class UserName < ActiveRecord::Migration
+  def up
+  	change_table :users do |t|
+  		t.column :name, :string
+  	end
+  end
+
+  def down
+  	change_table :users do |t|
+  		t.remove :name
+  	end
+  end
+end
