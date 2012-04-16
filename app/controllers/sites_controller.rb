d853f82e924cfee2c6c6b8d6fc26671ee4b4e466
Pedro Merlo <p.merlofeter@gmail.com>
2012-04-16 23:16:03 +0200

    visitas añadidas, no se incrementan

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -2,6 +2,7 @@ class SitesController < ApplicationController
 
   # authenticate_user! ejecuta acción solo si sesión existe
   before_filter :authenticate_user!, :except => [ :index, :show ]
+  # after_filter :count_visita, :only => :show
   
   # GET /sites
   # GET /sites.json
@@ -86,6 +87,11 @@ class SitesController < ApplicationController
       format.html { redirect_to sites_url }
       format.json { head :no_content }
     end
+  end
+
+  private
+  def count_visita
+    @site.increment!(:visitas)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120410162208) do
+ActiveRecord::Schema.define(:version => 20120416205821) do
 
   create_table "comments", :force => true do |t|
     t.string   "comment"
@@ -26,9 +26,10 @@ ActiveRecord::Schema.define(:version => 20120410162208) do
     t.text     "description"
     t.integer  "type_id"
     t.string   "image_url"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
     t.integer  "user_id"
+    t.integer  "visitas",     :default => 0
   end
 
   create_table "trips", :force => true do |t|
