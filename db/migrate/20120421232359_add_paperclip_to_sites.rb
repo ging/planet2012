a699180ffe6c002b24ebad17e907c3f72f91e01a
Merlin <merlin@MacBook-Air-de-Merlin.local>
2012-04-22 01:33:40 +0200

    Añadido Paperclip para la subida de imagenes

---
+++ db/migrate/20120421232359_add_paperclip_to_sites.rb
@@ -0,0 +1,20 @@
+class AddPaperclipToSites < ActiveRecord::Migration
+  def up
+   change_table :sites do |t|
+     t.column :image_file_name, :string
+     t.column :image_content_type, :string
+     t.column :image_file_size, :string
+     t.column :image_updated_at, :datetime
+   end
+  end
+
+  def down
+   change_table :sites do |t|
+     t.remove :image_file_name, :string
+     t.remove :image_content_type, :string
+     t.remove :image_file_size, :string
+     t.remove :image_updated_at, :datetime
+   end
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120418184242) do
+ActiveRecord::Schema.define(:version => 20120421232359) do
 
   create_table "comentarios", :force => true do |t|
     t.string   "coment"
@@ -26,12 +26,16 @@ ActiveRecord::Schema.define(:version => 20120418184242) do
     t.text     "description"
     t.integer  "type_id"
     t.string   "image_url"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
     t.integer  "user_id"
     t.float    "latitud"
     t.float    "longitud"
     t.integer  "zoom"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.string   "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "trips", :force => true do |t|
