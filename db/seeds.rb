5aa487a639ab356825362b17d0549a138ed164e0
Eduardo Gonzalez Martin <eduardo.gonzalez.martin@alumnos.upm.es>
2012-04-24 23:59:58 +0200

    Arreglado seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,42 +41,62 @@ Site.delete_all
 
 site1= Site.create!(
             name: 'Pedriza',
+			latitud: '40.734',
+			longitud: '-3.873',
+			zoom: '4',
             description: 'Magnifico valle al norte de Madrid en el Macizo Central',
             type_id: type2.id,
             image_url: 'pedriza.png',
             image: File.open(File.join(Rails.root, 'app', 'assets','images', 'pedriza.png'), "r"))
+			
 site1.user_id = user1.id ; site1.save 
 
 site2 = Site.create!(
             name: 'Catedral de Florencia',
+			latitud: '43.7675',
+			longitud: '11.2526',
+			zoom: '4',
             description: 'Catedral de la ciudad de Florencia con la que se inicia el Renacimiento',
             type_id: type1.id,
             image_url: 'florencia.png',
             image: File.open(File.join(Rails.root, 'app', 'assets','images', 'florencia.png'), "r"))
+
 site2.user_id = user1.id ; site2.save 
 
 site3 = Site.create!(
             name: 'Jardin de Lineo',
+			latitud: '59.86225',
+			longitud: '17.63426',
+			zoom: '4',
             description: 'Jardin de la ciudad sueca de Uppsala donde el famoso naturalista tenia su coleccion de plantas',
             type_id: type1.id,
             image_url: 'arbol1.png',
             image: File.open(File.join(Rails.root, 'app', 'assets','images', 'arbol1.png'), "r"))
+
 site3.user_id = user2.id ; site3.save 
 
 site4 = Site.create!(
             name: 'Reichstag',
+			latitud: '52.51904',
+			longitud: '13.3754',
+			zoom: '4',
             description: 'Parlamento aleman en la ciudad de Berlin',
             type_id: type2.id,
             image_url: 'reichstag.png',
             image: File.open(File.join(Rails.root, 'app', 'assets','images', 'reichstag.png'), "r"))
+
 site4.user_id = user2.id ; site4.save 
 
 site5 = Site.create!(
             name: 'Pergamo',
+			latitud: '35.4775',
+			longitud: '23.8663',
+			zoom: '4',
             description: 'Puerta del mercado de la antigua ciudad griega de Pergamo del museo arquelogico de Berlin',
             type_id: type3.id,
             image_url: 'pergamo.png',
             image: File.open(File.join(Rails.root, 'app', 'assets','images', 'pergamo.png'), "r"))
+
 site5.user_id = user1.id ; site5.save 
 
 
