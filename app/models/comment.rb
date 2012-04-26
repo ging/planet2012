caec5738818b19058583cd2b71897964b6fba588
tpais <paisruivo@gmail.com>
2012-04-26 11:46:23 +0200

    actualizaciones entregas

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :site
+
+	# se limita a 240 caracteres
+	validates :comment, :length => {:maximum => 240}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -56,6 +56,9 @@ site1= Site.create!(
             description: 'Magnifico valle al norte de Madrid en el Macizo Central',
             type_id: type2.id,
             image_url: 'pedriza.png',
+            latitud: 40.727054,
+            longitud: -3.8649140999999645,
+            zoom: 15,
             image: File.open(File.join(Rails.root, 'app', 'assets','images', 'pedriza.png'), "r"))
 site1.user_id = user1.id ; site1.save 
 
@@ -64,6 +67,9 @@ site2 = Site.create!(
             description: 'Catedral de la ciudad de Florencia con la que se inicia el Renacimiento',
             type_id: type1.id,
             image_url: 'florencia.png',
+            latitud: 43.772876,
+            longitud: 11.255798,
+            zoom: 15,
             image: File.open(File.join(Rails.root, 'app', 'assets','images', 'florencia.png'), "r"))
 site2.user_id = user1.id ; site2.save 
 
@@ -72,6 +78,9 @@ site3 = Site.create!(
             description: 'Jardin de la ciudad sueca de Uppsala donde el famoso naturalista tenia su coleccion de plantas',
             type_id: type1.id,
             image_url: 'arbol1.png',
+            latitud: 59.8622347,
+            longitud: 17.6341485,
+            zoom: 15,
             image: File.open(File.join(Rails.root, 'app', 'assets','images', 'arbol1.png'), "r"))
 site3.user_id = user2.id ; site3.save 
 
@@ -80,6 +89,9 @@ site4 = Site.create!(
             description: 'Parlamento aleman en la ciudad de Berlin',
             type_id: type2.id,
             image_url: 'reichstag.png',
+            latitud: 52.5186516,
+            longitud: 13.3759899,
+            zoom: 15,
             image: File.open(File.join(Rails.root, 'app', 'assets','images', 'reichstag.png'), "r"))
 site4.user_id = user2.id ; site4.save 
 
@@ -88,6 +100,9 @@ site5 = Site.create!(
             description: 'Puerta del mercado de la antigua ciudad griega de Pergamo del museo arquelogico de Berlin',
             type_id: type3.id,
             image_url: 'pergamo.png',
+            latitud: 52.5212514,
+            longitud: 13.3969041,
+            zoom: 15,
             image: File.open(File.join(Rails.root, 'app', 'assets','images', 'pergamo.png'), "r"))
 site5.user_id = user1.id ; site5.save 
 
